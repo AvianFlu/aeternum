2fbfd45f75323f695a7dd0f88257dabfd7ec3c11
Charlie McConnell <charlie@charlieistheman.com>
2012-05-15 09:43:41 -0700

    [refactor] Add aeternum_dup(), improve usability as a static library.

--- aeternum.h
+++ aeternum.h
@@ -1,3 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #ifndef _aeternum_h
 #define _aeternum_h
 
@@ -7,10 +11,16 @@ options_t aeternum_options(int argc, char *argv[]);
 
 void aeternum_start(options_t opts);
 
-void aeternum_fork();
+int aeternum_fork();
 
-void aeternum_redirect(const char *filename, int fileno);
+int aeternum_redirect(const char *filename, int fileno);
+
+int aeternum_dup(int oldfd, int newfd);
 
 void aeternum_exec(const char *filename, char *args[]);
 
+#endif
+
+#ifdef __cplusplus
+}
 #endif
